28cae75ae1c08a5c26f9bcae766144bbd1270c93
Munise Ben <munise0704@gmail.com>
2022-11-04 14:30:47 +0100

    new

--- README.md
+++ README.md
@@ -2,9 +2,17 @@
 
 
 
-## What are the codes in?
+## How i started?
 
-- loading
+- First I created a template in my head.
+
+![Start](proje.jpg)
+
+## How's it going?
+
+- It looks more or less like this right now.
+
+![End](proje2.png)
 
 
 ![yeah](https://media.giphy.com/media/KzM1lAfJjCWNq/giphy.gif)
--- script.js
+++ script.js
@@ -1,35 +1,44 @@
-//1
+let myArray = [];
+
+//1 - task1
 const add_task=()=>{
-    let task= document.getElementById("add_task").value;
-    return document.getElementById("task1").innerHTML= "You have selected " + task;
+    let task1= document.getElementById("task1");
+    let add_task = document.getElementById('add_task').value;
+    task1.innerHTML = "You have selected " + add_task;
+    myArray.push(task1.innerHTML);  //console.log(task1);
  }
 
-//2
+//2 - desc1
 const desc_task=()=>{
-    let desc_task= document.getElementById("desc_task").value;
-    return document.getElementById("desc1").innerHTML= "You have selected " +desc_task;
+    let desc1= document.getElementById("desc1");
+    let desc_task =document.getElementById('desc_task').value;
+    desc1.innerHTML = 'This project is about '+ desc_task;
+    myArray.push(desc1.innerHTML)
 
  }
 
-//3
-function getDays(){
+//3 - days
+const get_days=()=>{
 
+    let days = document.getElementById("days");
     var start_date = new Date(document.getElementById('start_date').value);
     var end_date = new Date(document.getElementById('end_date').value);
     //Here we will use getTime() function to get the time difference
     var time_difference = end_date.getTime() - start_date.getTime();
     //Here we will divide the above time difference by the no of miliseconds in a day
     var days_difference = time_difference / (1000*3600*24);
-    //alert(days);
-
     document.getElementById('days').innerHTML += days_difference;
+    myArray.push(days.innerHTML)
   }
 
 //https://biplabsinha345.medium.com/how-to-calculate-javascript-difference-between-two-dates-in-days-210eeebf3bb
 
-// 4 // iki secim yapinca calismiyor?
+// 4 - result // iki secim yapinca calismiyor?
 
-function getCheckboxValue() {
+const getCheckboxValue=()=>{
+
+
+  // var result=document.getElementById('result')
   var todo= document.getElementById("todo");
   var doing= document.getElementById("doing");
   var done= document.getElementById("done");
@@ -48,47 +57,25 @@ function getCheckboxValue() {
     result= result + lg3 ;
   }
    else {
-  return document.getElementById("result").innerHTML= "Select any one";
-  }
-  return document.getElementById("result").innerHTML= "You have selected " + result;
+  myArray.push(document.getElementById("result").innerHTML= "Select any one"
+  )}
+  myArray.push(document.getElementById("result").innerHTML= "You have selected " + result)
 }
 
-//5
-// const div = document.createElement('div');
-// const body = document.querySelector('body')
-// const addAll=()=>{
-//   let task= 'hjhjjh';
-//   body.appendChild(div)
-//   div.innerText=task
-// }
+//5 - add_all
+const body = document.querySelector('body')
+var show = document.getElementById('show');
+var section = document.createElement('section')
+const add_all=()=>{
+  var section =
+  document.createElement('div')
+  body.appendChild(div)
+  div.innerHTML= myArray
+
+    // console.log(myArray)
+ }
 
 
-const body = document.querySelector('body');
-const buttonAll = document.getElementById("buttonAll");
-let task1 = document.getElementById("task1")
-let desc1 = document.getElementById('desc1')
-let days = document.getElementById('days')
-let result = document.getElementById('result')
-
-
-  buttonAll.addEventListener('click',()=>{
-    const ul = document.createElement ('ul');
-    body.appendChild(ul)
-    ul.innerHTML=((JSON.stringify(task1)) +desc1 + days + result )
-    })
-
-
-
-    // const foo = {
-    //   foundation: "Mozilla",
-    //   model: "box",
-    //   week: 45,
-    //   transport: "car",
-    //   month: 7,
-    // };
-
-    // JSON.stringify(foo, ["week", "month"]);
-    // '{"week":45,"month":7}', only keep "week" and "month" properties
 
     //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
 
